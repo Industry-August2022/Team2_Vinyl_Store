3de282378a81c683807505e42dcfd1abfb47d55b
Myles <042Selim@gmail.com>
2022-10-06 15:26:54 -0400

    Add DevTools and work on ArtistController

--- src/main/java/team2/vinyl_store/rest_controllers/ArtistController.java
+++ src/main/java/team2/vinyl_store/rest_controllers/ArtistController.java
@@ -1,8 +1,8 @@
 package team2.vinyl_store.rest_controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import team2.vinyl_store.Artist;
@@ -10,16 +10,17 @@ import team2.vinyl_store.services.ArtistService;
 
 @RestController
 public class ArtistController {
-	
+
 	private final ArtistService artistService;
-	
+
 	@Autowired
 	public ArtistController(ArtistService artistService) {
 		this.artistService = artistService;
 	}
-	
-	@PostMapping(path = "/api/artist/{id}")
-	public Artist getById(@RequestParam int id) {
+
+	@GetMapping(path = "/api/artist/{id}")
+	public Artist getById(@PathVariable int id) {
+		System.out.println("GETBYID");
 		return artistService.getArtistByID(id);
 	}
 
