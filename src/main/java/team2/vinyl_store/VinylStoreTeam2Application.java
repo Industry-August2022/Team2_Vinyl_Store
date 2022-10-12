a2fe269feb1636062889849afa66eed206a987d1
dnsimms <38442548+dnsimms@users.noreply.github.com>
2022-10-12 12:14:34 -0400

    forgot to stage these

--- src/main/java/team2/vinyl_store/VinylStoreTeam2Application.java
+++ src/main/java/team2/vinyl_store/VinylStoreTeam2Application.java
@@ -2,6 +2,9 @@ package team2.vinyl_store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class VinylStoreTeam2Application {
@@ -9,5 +12,17 @@ public class VinylStoreTeam2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(VinylStoreTeam2Application.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/*").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/api/*/*").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/api/*/*/*").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 
 }
