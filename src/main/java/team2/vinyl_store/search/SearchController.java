a3546124d3e2249d3a9751525eca77b3cfff62eb
Myles <042Selim@gmail.com>
2022-10-13 12:46:06 -0400

    Implement searching and filtering

---
+++ src/main/java/team2/vinyl_store/search/SearchController.java
@@ -0,0 +1,122 @@
+package team2.vinyl_store.search;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import team2.vinyl_store.Vinyl;
+import team2.vinyl_store.services.ArtistService;
+import team2.vinyl_store.services.GenreService;
+import team2.vinyl_store.services.StudioService;
+import team2.vinyl_store.services.VinylService;
+
+@RestController
+public class SearchController {
+
+	private final VinylService vinylService;
+	private final GenreService genreService;
+	private final ArtistService artistService;
+	private final StudioService studioService;
+	
+	private final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+	@Autowired
+	public SearchController(VinylService vinylService, GenreService genreService, ArtistService artistService,
+			StudioService studioService) {
+		this.vinylService = vinylService;
+		this.genreService = genreService;
+		this.artistService = artistService;
+		this.studioService = studioService;
+	}
+
+	@PostMapping(path = "/api/search/vinyl")
+	public List<Vinyl> searchVinyl(@RequestBody(required = false) SearchVinylRequest searchRequest) {
+		List<Vinyl> vinyls = vinylService.getAllVinyl();
+		if (searchRequest == null)
+			return vinyls;
+
+		List<Vinyl> ret = new LinkedList<>();
+		for (Vinyl vinyl : vinyls) {
+			if (searchRequest.getQuery() != null && isInQuery(vinyl, searchRequest.getQuery()))
+				ret.add(vinyl);
+			if (vinyl.getArtistId() == searchRequest.getArtistId())
+				ret.add(vinyl);
+			if (vinyl.getGenreId() == searchRequest.getGenreId())
+				ret.add(vinyl);
+			if (vinyl.getStudioId() == searchRequest.getStudioId())
+				ret.add(vinyl);
+		}
+
+		if (searchRequest.getOrderBy() != null)
+			orderResults(ret, searchRequest.getOrderBy());
+		return ret;
+	}
+	
+	private void orderResults(List<Vinyl> vinyl, String orderBy) {
+		switch (orderBy) {
+		case "RUNTIME_HIGH_LOW":
+			vinyl.sort((v1, v2) -> v1.getRuntime() > v2.getRuntime() ? -1 : 1);
+			break;
+		case "RUNTIME_LOW_HIGH":
+			vinyl.sort((v1, v2) -> v1.getRuntime() < v2.getRuntime() ? -1 : 1);
+			break;
+		case "PRICE_HIGH_LOW":
+			vinyl.sort((v1, v2) -> v1.getPriceCents() > v2.getPriceCents() ? -1 : 1);
+			break;
+		case "PRICE_LOW_HIGH":
+			vinyl.sort((v1, v2) -> v1.getPriceCents() < v2.getPriceCents() ? -1 : 1);
+			break;
+		case "DATE_RECENT":
+			vinyl.sort((v1, v2) -> {
+				boolean isBefore;
+				try {
+					isBefore = DATE_FORMAT.parse(v1.getReleaseDate()).before(DATE_FORMAT.parse(v2.getReleaseDate()));
+					return isBefore ? -1 : 1;
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				return 0;
+			});
+			break;
+		case "DATE_OLD":
+			vinyl.sort((v1, v2) -> {
+				boolean isBefore;
+				try {
+					isBefore = !DATE_FORMAT.parse(v1.getReleaseDate()).before(DATE_FORMAT.parse(v2.getReleaseDate()));
+					return isBefore ? -1 : 1;
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				return 0;
+			});
+			break;
+		}
+	}
+
+	private boolean isInQuery(Vinyl vinyl, String query) {
+		String[] queryParts = query.split(" ");
+		for (int i = 0; i < queryParts.length; i++)
+			queryParts[i] = queryParts[i].toLowerCase();
+
+		String[] vinylSeachStrings = new String[] { vinyl.getTitle().toLowerCase(),
+				vinyl.getDescription().toLowerCase(),
+				genreService.getGenreByID(vinyl.getGenreId()).getName().toLowerCase(),
+				artistService.getArtistByID(vinyl.getArtistId()).getName().toLowerCase(),
+				studioService.getStudioByID(vinyl.getStudioId()).getName().toLowerCase() };
+
+		for (String part : queryParts) {
+			for (String searchString : vinylSeachStrings) {
+				if (searchString.contains(part))
+					return true;
+			}
+		}
+		return false;
+	}
+
+}
---
+++ src/main/java/team2/vinyl_store/search/SearchVinylRequest.java
@@ -0,0 +1,74 @@
+package team2.vinyl_store.search;
+
+import java.util.Comparator;
+
+import team2.vinyl_store.Vinyl;
+
+public class SearchVinylRequest {
+
+	private String query;
+	private int artistId;
+	private int genreId;
+	private int studioId;
+	private String orderBy;
+
+	public SearchVinylRequest() {
+		super();
+	}
+
+	public SearchVinylRequest(String query, int artistId, int genreId, int studioId, String orderBy) {
+		super();
+		this.query = query;
+		this.artistId = artistId;
+		this.genreId = genreId;
+		this.studioId = studioId;
+		this.orderBy = orderBy;
+	}
+
+	@Override
+	public String toString() {
+		return "SearchRequest [query=" + query + ", artistId=" + artistId + ", genreId=" + genreId + ", studioId="
+				+ studioId + ", orderBy=" + orderBy + "]";
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	public int getArtistId() {
+		return artistId;
+	}
+
+	public void setArtistId(int artistId) {
+		this.artistId = artistId;
+	}
+
+	public int getGenreId() {
+		return genreId;
+	}
+
+	public void setGenreId(int genreId) {
+		this.genreId = genreId;
+	}
+
+	public int getStudioId() {
+		return studioId;
+	}
+
+	public void setStudioId(int studioId) {
+		this.studioId = studioId;
+	}
+
+	public String getOrderBy() {
+		return orderBy;
+	}
+
+	public void setOrderBy(String orderBy) {
+		this.orderBy = orderBy;
+	}
+
+}
