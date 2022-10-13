2c0aac210a3e7954fd90c4b6de71232141a86516
Myles <042Selim@gmail.com>
2022-10-13 11:40:08 -0400

    Allow genres in home navbar autopopulate

--- vinyl_store_front/src/app/home/home.component.ts
+++ vinyl_store_front/src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 import { UserService } from '../services/user.service';
 import { first } from 'rxjs';
+import { Genre } from '../api-classes/genre';
+import { GenreService } from '../services/genre.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,11 +14,13 @@ export class HomeComponent implements OnInit {
 
   currentUser: any;
   users: any[] = [];
+  genres !: Genre[];
 
   constructor(
     private router:Router,
     private authService: AuthenticationService,
-    private userService: UserService
+    private userService: UserService,
+    private genreService: GenreService
   ){
     this.authService.currentUser.subscribe(x => this.currentUser = x);
   }
@@ -34,7 +38,13 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadAllUsers();
-  
+
+    this.genreService.getAllGenres().subscribe(
+      response => {
+        this.genres = response;
+        this.genres.sort((a, b) => a.genreId < b.genreId?-1:1);
+      }
+    );
   }
 
 }
